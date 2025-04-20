3fd3ea317547fa4f871508f7f61b1571bd8b71bd
bjoernwenderoth <bjoernwe1989@googlemail.com>
2025-04-20 20:47:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let gruende = [
+    'Egal,',
+    'was du hier ',
+    'eingibst,',
+    'der einzige Grund,',
+    'warum ich dich liebe',
+    'Hasi,',
+    'ist,',
+    'dass du ein',
+    'TOLLER MENSCH',
+    'bist!',
+    ':) ❤️',
+];
+
+function ausgabe() {
+    let outputContainer = document.getElementById('outputContainer');
+    outputContainer.innerHTML = '';
+
+    for (let i = 0; i < gruende.length; i++) {
+        let grund = gruende[i];
+        setTimeout(() => {
+            outputContainer.innerHTML += `<span>${grund} </span>`;
+        }, i * 1500);
+    }
+}
